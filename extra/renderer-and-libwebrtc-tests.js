2882cf664080be7ed0958e5f0a489586fe2ee221
Harold Thetiot <hthetiot@gmail.com>
2019-09-04 23:29:41 +0200

    update extra/renderer-and-libwebrtc-tests.js with setRemoteDescription|setRemoteDescription without RTCSessionDescription

--- extra/renderer-and-libwebrtc-tests.js
+++ extra/renderer-and-libwebrtc-tests.js
@@ -215,13 +215,19 @@ function TestRTCPeerConnection(localStream) {
     return pc1.createOffer().then(function (d) {
       return pc1.setLocalDescription(d);
     }).then(function () {
-      return pc2.setRemoteDescription(pc1.localDescription);
+      return pc2.setRemoteDescription({
+        type: pc1.localDescription.type,
+        sdp: pc1.localDescription.sdp
+      });
     }).then(function () {
       return pc2.createAnswer();
     }).then(function (d) {
       return pc2.setLocalDescription(d);
     }).then(function () {
-      return pc1.setRemoteDescription(pc2.localDescription);
+      return pc1.setRemoteDescription({
+        type: pc2.localDescription.type,
+        sdp: pc2.localDescription.sdp
+      });
     }).catch(function (err) {
       console.log('createOfferError', err);
     });
