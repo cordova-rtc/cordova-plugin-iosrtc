88a6152577bbee7d970620b30bc6546f1a8c4ab3
Harold Thetiot <hthetiot@gmail.com>
2019-09-12 19:59:34 +0200

    update js/EventTarget.js

--- js/EventTarget.js
+++ js/EventTarget.js
@@ -1,29 +1,12 @@
 /**
- * Expose the EventTarget class.
- */
-module.exports = EventTarget;
-
-/**
  * Dependencies.
  */
 var 
 	YaetiEventTarget = require('yaeti').EventTarget;
 
+var EventTarget = YaetiEventTarget;
+
 /**
  * Expose the EventTarget class.
  */
-function EventTarget() {
-
-	// Prevent yaeti doing nothing if called for a native EventTarget object..
-	if (typeof this.addEventListener === 'function') {
-		delete this.addEventListener;
-	}
-	
-	var eventTarget = YaetiEventTarget.call(this, arguments);
-
-	return eventTarget;
-}
-
-EventTarget.prototype = Object.create(YaetiEventTarget.prototype);
-
-EventTarget.prototype.constructor = EventTarget;
+module.exports = EventTarget;
