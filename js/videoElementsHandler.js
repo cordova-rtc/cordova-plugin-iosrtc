fef5bb458c430858b5b6ac7233b7147d9c08bfc4
Pelle Mattsson <pellem.92@gmail.com>
2019-10-02 21:04:33 +0200

    Recognize when a video receives a new srcObject and re-render the media stream

--- js/videoElementsHandler.js
+++ js/videoElementsHandler.js
@@ -201,6 +201,16 @@ function observeVideo(video) {
 		if (video.srcObject && !video._iosrtcMediaStreamRendererId) {
 			// If this video element was NOT previously handling a MediaStreamRenderer, release it.
 			handleVideo(video);
+		} else if (video.srcObject && video._iosrtcMediaStreamRendererId) {
+			// The video element has received a new srcObject.
+			const stream = video.srcObject;
+			if (stream && typeof stream.getBlobId === 'function') {
+				const mediaStreamRenderer = mediaStreamRenderers[video._iosrtcMediaStreamRendererId];
+				const mediaStream = mediaStreams[stream.getBlobId()];
+				if (mediaStreamRenderer && mediaStream) {
+					mediaStreamRenderer.render(mediaStream);
+				}
+			}
 		}
 	});
 
