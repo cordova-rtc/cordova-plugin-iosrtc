205e1a7dd005bce5ab5066e75a900ec9bd4bc39f
Harold Thetiot <hthetiot@gmail.com>
2019-10-06 15:54:12 +0200

    update extra/hooks/iosrtc-swift-support.js to only update .xcconfig and .pbxproj if needed

--- extra/hooks/iosrtc-swift-support.js
+++ extra/hooks/iosrtc-swift-support.js
@@ -10,8 +10,8 @@ var
 	path = require("path"),
 	xcode = require('xcode'),
 
-	BUILD_VERSION = '10.2',
-	BUILD_VERSION_XCODE = '"' + BUILD_VERSION + '"',
+	IPHONEOS_DEPLOYMENT_TARGET = '10.2',
+	IPHONEOS_DEPLOYMENT_TARGET_XCODE = '"' + IPHONEOS_DEPLOYMENT_TARGET + '"',
 	SWIFT_VERSION = '4.2',
 	SWIFT_VERSION_XCODE = '"' + SWIFT_VERSION + '"',
 	RUNPATH_SEARCH_PATHS = '@executable_path/Frameworks',
@@ -49,6 +49,51 @@ function nonComments(obj) {
 	return newObj;
 }
 
+function matchBuildSettingsValue(value, expectedValue) {
+	return value === expectedValue;
+}
+
+function hasBuildSettingsValue(value, expectedValue) {
+	return matchBuildSettingsValue(value, expectedValue) || value.indexOf(expectedValue) !== -1;
+}
+
+function convertToFloat(value) {
+	return value.replace(/[^\d.-]/g,'');
+}
+
+function matchMinValue(value, minValue) {
+	return convertToFloat(value) >= convertToFloat(minValue);
+}
+
+function matchBuildSettingsMinValue(value, expectedValue) {
+	return matchBuildSettingsValue(value, expectedValue) || matchMinValue(value, expectedValue);
+}
+
+function matchXcconfigPathValue(swiftOptions, path, expectedValue) {
+	return swiftOptions.filter(function (swiftOption) {
+		return swiftOption === path + ' = ' + expectedValue;
+	}).length > 0;
+}
+
+function matchXcconfigMinValue(swiftOptions, path, expectedValue) {
+	var swiftOption = swiftOptions.find(function (swiftOption) {
+		return swiftOption.split(' = ')[0] === path;
+	});
+
+	return swiftOption && matchMinValue(swiftOption.split(' = ')[1], expectedValue);
+}
+
+function getRelativeToProjectRootPath(path, projectRoot) {
+	return path.replace(projectRoot, '.');
+}
+
+function debug(msg) {
+	console.log('iosrtc-swift-support.js [INFO] ' + msg);
+}
+
+function debugerror(msg) {
+	console.error('iosrtc-swift-support.js [ERROR] ' + msg);
+}
 
 // Starting here
 
@@ -64,80 +109,129 @@ module.exports = function (context) {
 		swiftOptions = [''], // <-- begin to file appending AFTER initial newline
 		xcodeProject;
 
+	// Showing info about the tasks to do
+	debug('cordova-plugin-iosrtc is checking issues in the generated project files:');
+	debug('- Minimum "iOS Deployment Target" and "Deployment Target" to: ' + IPHONEOS_DEPLOYMENT_TARGET_XCODE);
+	debug('- "Runpath Search Paths" to: ' + RUNPATH_SEARCH_PATHS_XCODE);
+	debug('- "Objective-C Bridging Header" to: ' + swiftBridgingHeadXcode);
+	debug('- "ENABLE_BITCODE" set to: ' + ENABLE_BITCODE_XCODE);
+	debug('- "SWIFT_VERSION" set to: ' + SWIFT_VERSION_XCODE);
+
 	// Checking if the project files are in the right place
 	if (!fs.existsSync(xcodeProjectPath)) {
 		debugerror('an error occurred searching the project file at: "' + xcodeProjectPath + '"');
 
 		return;
 	}
-	debug('".pbxproj" project file found: ' + xcodeProjectPath);
+	debug('".pbxproj" project file found: ' + getRelativeToProjectRootPath(xcodeProjectPath, projectRoot));
 
 	if (!fs.existsSync(xcconfigPath)) {
 		debugerror('an error occurred searching the project file at: "' + xcconfigPath + '"');
 
 		return;
 	}
-	debug('".xcconfig" project file found: ' + xcconfigPath);
+	debug('".xcconfig" project file found: ' + getRelativeToProjectRootPath(xcconfigPath, projectRoot));
 
 	xcodeProject = xcode.project(xcodeProjectPath);
 
-	// Showing info about the tasks to do
-	debug('fixing issues in the generated project files:');
-	debug('- "iOS Deployment Target" and "Deployment Target" to: ' + BUILD_VERSION_XCODE);
-	debug('- "Runpath Search Paths" to: ' + RUNPATH_SEARCH_PATHS_XCODE);
-	debug('- "Objective-C Bridging Header" to: ' + swiftBridgingHeadXcode);
-	debug('- "ENABLE_BITCODE" set to: ' + ENABLE_BITCODE_XCODE);
-	debug('- "SWIFT_VERSION" set to: ' + SWIFT_VERSION_XCODE);
-
-
 	// Massaging the files
-
+	
 	// "build.xcconfig"
-	swiftOptions.push('LD_RUNPATH_SEARCH_PATHS = ' + RUNPATH_SEARCH_PATHS);
-	swiftOptions.push('SWIFT_OBJC_BRIDGING_HEADER = ' + swiftBridgingHead);
-	swiftOptions.push('IPHONEOS_DEPLOYMENT_TARGET = ' + BUILD_VERSION);
-	swiftOptions.push('ENABLE_BITCODE = ' + ENABLE_BITCODE);
-	swiftOptions.push('SWIFT_VERSION = ' + SWIFT_VERSION);
+	debug('checking file: ' + getRelativeToProjectRootPath(xcconfigPath, projectRoot));
+	var xcconfigPathChanged = false;
+
+	// Parsing it
+	var currentSwiftOptions = [];
+	if (fs.existsSync(xcconfigPath)) {
+		currentSwiftOptions = fs.readFileSync(xcconfigPath).toString().split("\n");
+	}
+
+	if (!matchXcconfigPathValue(currentSwiftOptions, 'LD_RUNPATH_SEARCH_PATHS', RUNPATH_SEARCH_PATHS)) {
+		swiftOptions.push('LD_RUNPATH_SEARCH_PATHS = ' + RUNPATH_SEARCH_PATHS);
+		xcconfigPathChanged = true;
+	}
+
+	if (!matchXcconfigPathValue(currentSwiftOptions, 'SWIFT_OBJC_BRIDGING_HEADER', swiftBridgingHead)) {
+		swiftOptions.push('SWIFT_OBJC_BRIDGING_HEADER = ' + swiftBridgingHead);
+		xcconfigPathChanged = true;
+	}
+
+	if (!matchXcconfigMinValue(currentSwiftOptions, 'IPHONEOS_DEPLOYMENT_TARGET', IPHONEOS_DEPLOYMENT_TARGET)) {
+		swiftOptions.push('IPHONEOS_DEPLOYMENT_TARGET = ' + IPHONEOS_DEPLOYMENT_TARGET);
+		xcconfigPathChanged = true;
+	}
+
+	if (!matchXcconfigPathValue(currentSwiftOptions, 'ENABLE_BITCODE', ENABLE_BITCODE)) {
+		swiftOptions.push('ENABLE_BITCODE = ' + ENABLE_BITCODE);
+		xcconfigPathChanged = true;
+	}
+
+	if (!matchXcconfigPathValue(currentSwiftOptions, 'SWIFT_VERSION', SWIFT_VERSION)) {
+		swiftOptions.push('SWIFT_VERSION = ' + SWIFT_VERSION);
+		xcconfigPathChanged = true;
+	}
+
 	// NOTE: Not needed
 	// swiftOptions.push('EMBEDDED_CONTENT_CONTAINS_SWIFT = YES');
-	fs.appendFileSync(xcconfigPath, swiftOptions.join('\n'));
-	debug('file correctly fixed: ' + xcconfigPath);
+	
+	if (xcconfigPathChanged) {
+		fs.appendFileSync(xcconfigPath, swiftOptions.join('\n'));
+		debug('file correctly fixed: ' + getRelativeToProjectRootPath(xcconfigPath, projectRoot));
+	} else {
+		debug('file is correct: ' + getRelativeToProjectRootPath(xcconfigPath, projectRoot));
+	}
 
 	// "project.pbxproj"
+	debug('checking file: ' + getRelativeToProjectRootPath(xcodeProjectPath, projectRoot));	
+
 	// Parsing it
 	xcodeProject.parse(function (error) {
-		var configurations, buildSettings;
 
 		if (error) {
-			debugerror('an error occurred during the parsing of the project file');
-
+			debugerror('an error occurred during the parsing of the project file: ' + xcodeProjectPath);
 			return;
 		}
 
-
-		configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection());
-		// Adding or changing the parameters we need
+		// Adding or changing the parameters only if we need
+		var buildSettingsChanged = false;
+		var configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection());
 		Object.keys(configurations).forEach(function (config) {
-			buildSettings = configurations[config].buildSettings;
-			buildSettings.LD_RUNPATH_SEARCH_PATHS = RUNPATH_SEARCH_PATHS_XCODE;
-			buildSettings.SWIFT_OBJC_BRIDGING_HEADER = swiftBridgingHeadXcode;
-			buildSettings.IPHONEOS_DEPLOYMENT_TARGET = BUILD_VERSION_XCODE;
-			buildSettings.ENABLE_BITCODE = ENABLE_BITCODE_XCODE;
-			buildSettings.SWIFT_VERSION = SWIFT_VERSION_XCODE;
+			var buildSettings = configurations[config].buildSettings;
+
+			// TODO check compatibilityVersion once on Cordova 5+
+
+			if (!hasBuildSettingsValue(buildSettings.LD_RUNPATH_SEARCH_PATHS, RUNPATH_SEARCH_PATHS_XCODE)) {
+				buildSettings.LD_RUNPATH_SEARCH_PATHS = RUNPATH_SEARCH_PATHS_XCODE;
+				buildSettingsChanged = true;
+			}
+
+			if (!hasBuildSettingsValue(buildSettings.SWIFT_OBJC_BRIDGING_HEADER, swiftBridgingHeadXcode)) {
+				buildSettings.SWIFT_OBJC_BRIDGING_HEADER = swiftBridgingHeadXcode;	
+				buildSettingsChanged = true;
+			}
+
+			if (!matchBuildSettingsMinValue(buildSettings.IPHONEOS_DEPLOYMENT_TARGET, IPHONEOS_DEPLOYMENT_TARGET_XCODE)) {
+				buildSettings.IPHONEOS_DEPLOYMENT_TARGET = IPHONEOS_DEPLOYMENT_TARGET_XCODE;
+				buildSettingsChanged = true;				
+			}
+
+			if (!matchBuildSettingsValue(buildSettings.ENABLE_BITCODE, ENABLE_BITCODE_XCODE)) {
+				buildSettings.ENABLE_BITCODE = ENABLE_BITCODE_XCODE;
+				buildSettingsChanged = true;
+			}
+
+			if (!matchBuildSettingsValue(buildSettings.SWIFT_VERSION, SWIFT_VERSION_XCODE)) {
+				buildSettings.SWIFT_VERSION = SWIFT_VERSION_XCODE;
+				buildSettingsChanged = true;
+			}
 		});
 
-		// Writing the file again
-		fs.writeFileSync(xcodeProjectPath, xcodeProject.writeSync(), 'utf-8');
-		debug('file correctly fixed: ' + xcodeProjectPath);
+		// Writing the file only if changed
+		if (buildSettingsChanged) {
+			fs.writeFileSync(xcodeProjectPath, xcodeProject.writeSync(), 'utf-8');
+			debug('file correctly fixed: ' + getRelativeToProjectRootPath(xcodeProjectPath, projectRoot));	
+		} else {
+			debug('file is correct: ' + getRelativeToProjectRootPath(xcodeProjectPath, projectRoot));	
+		}
 	});
 };
-
-
-function debug(msg) {
-	console.log('iosrtc-swift-support.js [INFO] ' + msg);
-}
-
-
-function debugerror(msg) {
-	console.error('iosrtc-swift-support.js [ERROR] ' + msg);
-}
