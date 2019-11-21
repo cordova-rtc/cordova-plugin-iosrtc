4c50a575b1fc988d57ffc9589cdc323613f6c7b4
Harold Thetiot <hthetiot@gmail.com>
2019-11-21 21:21:17 +0100

    update dist/cordova-plugin-iosrtc.js build for 6.0.4

--- dist/cordova-plugin-iosrtc.js
+++ dist/cordova-plugin-iosrtc.js
@@ -1,5 +1,5 @@
 /*
- * cordova-plugin-iosrtc v6.0.3
+ * cordova-plugin-iosrtc v6.0.4
  * Cordova iOS plugin exposing the full WebRTC W3C JavaScript APIs
  * Copyright 2015-2017 eFace2Face, Inc. (https://eface2face.com)
  * Copyright 2015-2019 BasqueVoIPMafia (https://github.com/BasqueVoIPMafia)
