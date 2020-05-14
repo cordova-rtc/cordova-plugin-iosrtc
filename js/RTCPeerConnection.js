486577ff2cffe1de3a4314726cd28a7f9a8781a4
Harold Thetiot <hthetiot@gmail.com>
2020-05-14 14:05:11 +0200

    add ontrack RTCPeerConnection setter on SHIM and rename addtrack to track

--- js/RTCPeerConnection.js
+++ js/RTCPeerConnection.js
@@ -101,6 +101,13 @@ Object.defineProperties(RTCPeerConnection.prototype, {
 			return this.addEventListener('addstream', callback);
 		}
 	},
+	'ontrack': {
+		// Fix webrtc-adapter TypeError: Attempting to change the getter of an unconfigurable property.
+		configurable: true,
+		set: function (callback) {
+			return this.addEventListener('track', callback);
+		}
+	},
 	'oniceconnectionstatechange': {
 		// Fix webrtc-adapter TypeError: Attempting to change the getter of an unconfigurable property.
 		configurable: true,
@@ -701,7 +708,7 @@ function onEvent(data) {
 		case 'negotiationneeded':
 			break;
 
-		case 'addtrack':
+		case 'track':
 			event.track = data.track;
 			break;
 
