6b657ff091921b2b6bae4856419b4167034e0f86
Harold Thetiot <hthetiot@gmail.com>
2020-05-20 19:38:19 +0200

    fix undefined is not an object (evaluating 'stream.emitConnected')

--- js/RTCPeerConnection.js
+++ js/RTCPeerConnection.js
@@ -728,7 +728,7 @@ function onEvent(data) {
 
 			// Emit "connected" on the stream if ICE connected.
 			if (this.iceConnectionState === 'connected' || this.iceConnectionState === 'completed') {
-				stream.emitConnected();
+				event.stream.emitConnected();
 			}
 			break;
 
