520c583a2834aa132e8f3d0846a66dcbe4f642ee
Harold Thetiot <hthetiot@gmail.com>
2020-07-17 18:36:54 +0200

    fix tab indent PluginRTCPeerConnection.swift

--- src/PluginRTCPeerConnection.swift
+++ src/PluginRTCPeerConnection.swift
@@ -629,25 +629,26 @@ class PluginRTCPeerConnection : NSObject, RTCPeerConnectionDelegate {
 
 		NSLog("PluginRTCPeerConnection | onaddtrack")
 
-			// TODO investigate why no stream sometimes and confirm still the case.
-	        let pluginMediaTrack = PluginMediaStreamTrack(rtcMediaStreamTrack: rtpReceiver.track!)
+			let pluginMediaTrack = PluginMediaStreamTrack(rtcMediaStreamTrack: rtpReceiver.track!)
 
-	        // Add stream only if available in case of Unified-Plan of track event without stream
-	        if (streams.isEmpty) {
-	            self.eventListener([
-	                "type": "track",
-	                "track": pluginMediaTrack.getJSON(),
-	            ])
-	        } else {
-	            let pluginMediaStream = getPluginMediaStream(stream: streams[0]);
+			// Add stream only if available in case of Unified-Plan of track event without stream
+			// TODO investigate why no stream sometimes with Unified-Plan and confirm that expexted behavior.
 
-	            self.eventListener([
-	                "type": "track",
-	                "track": pluginMediaTrack.getJSON(),
-	                "streamId": pluginMediaStream!.id,
-	                "stream": pluginMediaStream!.getJSON()
-	            ])
-	        }
+			if (streams.isEmpty) {
+				self.eventListener([
+					"type": "track",
+					"track": pluginMediaTrack.getJSON(),
+				])
+			} else {
+				let pluginMediaStream = getPluginMediaStream(stream: streams[0]);
+
+				self.eventListener([
+					"type": "track",
+					"track": pluginMediaTrack.getJSON(),
+					"streamId": pluginMediaStream!.id,
+					"stream": pluginMediaStream!.getJSON()
+				])
+			}
 	}
 
 	/** Called when the SignalingState changed. */
