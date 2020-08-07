1124a047114d0284c1fddc60bd677fd369c2df34
Harold Thetiot <hthetiot@gmail.com>
2020-08-07 14:02:30 +0200

    update src/cordova-plugin-iosrtc-Bridging-Header.h to use WebRTC pod

--- src/cordova-plugin-iosrtc-Bridging-Header.h
+++ src/cordova-plugin-iosrtc-Bridging-Header.h
@@ -5,21 +5,5 @@
 
 #import <Cordova/CDVPlugin.h>
 
-#import <WebRTC/RTCAudioSource.h>
-#import <WebRTC/RTCAudioTrack.h>
-#import <WebRTC/RTCDataChannel.h>
-#import <WebRTC/RTCDtmfSender.h>
-#import <WebRTC/RTCEAGLVideoView.h>
-#import <WebRTC/RTCIceCandidate.h>
-#import <WebRTC/RTCIceServer.h>
-#import <WebRTC/RTCMediaConstraints.h>
-#import <WebRTC/RTCMediaSource.h>
-#import <WebRTC/RTCMediaStream.h>
-#import <WebRTC/RTCMediaStreamTrack.h>
-#import <WebRTC/RTCPeerConnection.h>
-#import <WebRTC/RTCPeerConnectionFactory.h>
-#import <WebRTC/RTCSessionDescription.h>
-#import <WebRTC/RTCVideoCapturer.h>
-#import <WebRTC/RTCVideoRenderer.h>
-#import <WebRTC/RTCVideoSource.h>
-#import <WebRTC/RTCVideoTrack.h>
+
+@import WebRTC;
