ded470fdb7c4ee62d5795cd0af52c64449ab63fe
Harold Thetiot <hthetiot@gmail.com>
2020-09-11 12:51:05 +0200

    remove i386 and armv7 from extra/ios_arch.js

--- extra/ios_arch.js
+++ extra/ios_arch.js
@@ -7,7 +7,7 @@ const path = require('path');
 const exec = require('child_process').execSync;
 
 const WEBRTC_BIN_PATH = path.join(__dirname, '../lib/WebRTC.framework');
-const ARCH_TYPES = ['i386', 'x86_64', 'armv7', 'arm64'];
+const ARCH_TYPES = ['x86_64', 'arm64'];
 
 /* === Example to strip simulator archs for Apple Store Submission ===
  *
@@ -37,11 +37,11 @@ if (process.argv[2] === '--extract' || process.argv[2] === '-e') {
 } else if (process.argv[2] === '--simulator' || process.argv[2] === '-s') {
   // re-package simulator related archs only. ( i386, x86_64 )
   console.log('Compiling simulator...');
-  exec(`lipo -o WebRTC -create WebRTC-x86_64 WebRTC-i386`, {cwd: WEBRTC_BIN_PATH});
+  exec(`lipo -o WebRTC -create WebRTC-x86_64`, {cwd: WEBRTC_BIN_PATH});
 } else if (process.argv[2] === '--device' || process.argv[2] === '-d') {
   // re-package device related archs only. ( armv7, arm64 )
   console.log('Compiling device...');
-  exec(`lipo -o WebRTC -create WebRTC-armv7 WebRTC-arm64`, {cwd: WEBRTC_BIN_PATH});
+  exec(`lipo -o WebRTC -create WebRTC-arm64`, {cwd: WEBRTC_BIN_PATH});
 } else if (process.argv[2] === '--list' || process.argv[2] === '-l') {
   // List WebRTC architectures
   console.log('List WebRTC architectures...');
