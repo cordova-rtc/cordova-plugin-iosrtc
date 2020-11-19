44eb3938d7f3ec0a0a6a65d726f6948c63ba0ccc
samgabriel <sgabriel@brainfuse.com>
2020-11-18 22:09:30 -0500

    Fixed the streamid regenerated with a provided id
    Fixed a Null error with the eventListener

--- src/PluginMediaStream.swift
+++ src/PluginMediaStream.swift
@@ -21,7 +21,11 @@ class PluginMediaStream : NSObject {
 		if (streamId == nil) {
 			// Handle possible duplicate remote trackId with  janus or short duplicate name
 			// See: https://github.com/cordova-rtc/cordova-plugin-iosrtc/issues/432
-			self.id = rtcMediaStream.streamId + "_" + UUID().uuidString;
+			if (rtcMediaStream.streamId.count < 36) {
+				self.id = rtcMediaStream.streamId + "_" + UUID().uuidString;
+			} else {
+				self.id = rtcMediaStream.streamId;
+			}
 		} else {
 			self.id = streamId!;
 		}
