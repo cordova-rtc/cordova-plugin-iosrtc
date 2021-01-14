8d45a81123beb1ecc1d5e4c6e4e4d8d58c10ba11
Harold Thetiot <hthetiot@gmail.com>
2021-01-14 13:34:34 +0100

    fix navigator.mediaDevices.GetUserMedia webrtc-adapter causing overide of navigator.mediaDevices on ios 14.3

--- js/iosrtc.js
+++ js/iosrtc.js
@@ -198,6 +198,9 @@ function registerGlobals(doNotRestoreCallbacksSupport) {
 	navigator.mediaDevices.getUserMedia = getUserMedia;
 	navigator.mediaDevices.enumerateDevices = enumerateDevices;
 
+	// Prevent WebRTC-adapter to overide navigator.mediaDevices after shim is applied since ios 14.3
+	Object.freeze(navigator.mediaDevices);
+
 	window.RTCPeerConnection = RTCPeerConnection;
 	window.webkitRTCPeerConnection = RTCPeerConnection;
 	window.RTCSessionDescription = RTCSessionDescription;
