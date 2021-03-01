e6792ed9a2fab8c63ac60f0fa20c81a427c059db
Harold Thetiot <hthetiot@gmail.com>
2021-03-01 16:55:38 +0100

    merge master

--- js/iosrtc.js
+++ js/iosrtc.js
@@ -193,20 +193,22 @@ function registerGlobals(doNotRestoreCallbacksSupport) {
 	navigator.webkitGetUserMedia = getUserMedia;
 
 	// Prevent WebRTC-adapter to overide navigator.mediaDevices after shim is applied since ios 14.3
-	Object.defineProperty(
-		navigator,
-		'mediaDevices',
-		{
-			value: new MediaDevices(),
-			writable: false
-		},
-		{
-			enumerable: false,
-			configurable: false,
-			writable: false,
-			value: 'static'
-		}
-	);
+	if (!(navigator.mediaDevices instanceof MediaDevices)) {
+		Object.defineProperty(
+			navigator,
+			'mediaDevices',
+			{
+				value: new MediaDevices(),
+				writable: false
+			},
+			{
+				enumerable: false,
+				configurable: false,
+				writable: false,
+				value: 'static'
+			}
+		);
+	}
 
 	window.RTCPeerConnection = RTCPeerConnection;
 	window.webkitRTCPeerConnection = RTCPeerConnection;
